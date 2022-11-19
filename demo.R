ac72c2682470bd84cdb58a015a44f42a7d657506
Arjun Radhakrishnan <arradhak@student.ubc.ca>
2022-11-19 15:37:05 -0800

    Reset R

--- demo.R
+++ demo.R
@@ -2,20 +2,15 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> [<new_arg>] --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
-<new_arg>         Takes any value (this is a optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
-main <- function(opt) {
-    print(opt)
-    print(typeof(opt))
-}
-
-main(opt)
+print(opt)
+print(typeof(opt))
