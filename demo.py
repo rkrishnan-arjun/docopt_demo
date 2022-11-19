5ab3873cb25d66a4203cf536ebbd37780ff275db
Arjun Radhakrishnan <arradhak@student.ubc.ca>
2022-11-19 15:35:24 -0800

    Py - Add optional positional argument

--- demo.py
+++ demo.py
@@ -2,15 +2,17 @@
 # date: 2020-01-15
 
 """This script prints out docopt args.
-Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.py <arg1> [<new_arg>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
-<arg1>             Takes any value (this is a required positional argument)
+<arg1>            Takes any value (this is a required positional argument)
+[<new_arg>]       Takes any value (this is a optional positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-""" 
+"""
 
 from docopt import docopt
+
 opt = docopt(__doc__)
 print(opt)
 print(type(opt))
